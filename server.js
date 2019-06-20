54018cb201f98b73cc52868df0f0378db20939d7
Andrei Kulagin <kulawork23@yandex.ru>
2019-06-20 15:50:31 +0400

    Deployment ready version

--- server.js
+++ server.js
@@ -1,20 +1,28 @@
-const express = require('express');
-const connectDB = require('./config/db')
+const express = require("express");
+const connectDB = require("./config/db");
 const app = express();
+const path = require("path");
 
 connectDB();
 
-app.use(express.json({
+app.use(
+  express.json({
     extended: false
-}));
+  })
+);
 
-app.get('/', (req, res) => res.send('API is running'));
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
 
-app.use('/api/users', require('./routes/api/users'));
-app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/profile', require('./routes/api/profile'));
-app.use('/api/posts', require('./routes/api/posts'));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
 
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
